aaf9ee1213e2c9c9b657d5883845ff96d3a5fe1e
Ripal Shah <ripal.1021@gmail.com>
2017-11-05 15:56:45 +0530

    Add new Function for Home page
    New function and changes in database server address

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
     session_start();
 
-    $link = mysqli_connect("localhost", "root", "", "twitter");
+    $link = mysqli_connect("127.0.0.1", "root", "", "twitter");
 
 if (mysqli_connect_error()) {
     
@@ -14,6 +14,68 @@ if (isset($_GET['function']) == "logout") {
     
     session_unset();
 }
+function time_since($since) {
+    $chunks = array(
+        array(60 * 60 * 24 * 365 , 'year'),
+        array(60 * 60 * 24 * 30 , 'month'),
+        array(60 * 60 * 24 * 7, 'week'),
+        array(60 * 60 * 24 , 'day'),
+        array(60 * 60 , 'hour'),
+        array(60 , 'min'),
+        array(1 , 's')
+    );
+
+    for ($i = 0, $j = count($chunks); $i < $j; $i++) {
+        $seconds = $chunks[$i][0];
+        $name = $chunks[$i][1];
+        if (($count = floor($since / $seconds)) != 0) {
+            break;
+        }
+    }
+
+    $print = ($count == 1) ? '1 '.$name : "$count {$name}s";
+    return $print;
+}
+
+function displayTweets($type) {
+    
+    global $link;
+    
+    if ($type == 'public') {
+        
+        $whereClause = "";
+        
+    }
+    
+    $query = "SELECT * FROM tweets ".$whereClause." ORDER BY 'datetime' DESC LIMIT 10";  
+    $result = mysqli_query($link, $query);
+    
+    if (mysqli_num_rows($result) == 0) {
+        
+        echo "There are no tweets to display.";
+        
+    } else {
+        
+        while ($row = mysqli_fetch_assoc($result)) {
+            
+            $userQuery = "SELECT * FROM users WHERE id = ".mysqli_real_escape_string($link, $row['userid'])." LIMIT 1";
+            $userQueryResult = mysqli_query($link, $userQuery);
+            $user = mysqli_fetch_assoc($userQueryResult);
+            
+            echo "<div class='tweet'><p>".$user['email']." <span class='time'>".time_since(time() - strtotime($row['datetime']))." ago</span>:</p>";
+            
+            echo "<p>".$row['tweet']."</p>";
+            
+                echo "<p>Follow</p></div>";
+        }
+    }
+        
+        
+}
+
+
+
+
 
 
 
--- views/home.php
+++ views/home.php
@@ -1,5 +1,13 @@
-<div class="container">   
+<div class="container mainCotainer">   
 
-    <h1>Home Page</h1>
+    <div class="row">
+  <div class="col-12 col-md-8">
+      
+      <h1>Recent Tweets</h1>
+      
+      <?php displayTweets('public');   ?>
+  </div>
+  <div class="col-6 col-md-4">.col-6 .col-md-4</div>
+</div>
 
  </div>
